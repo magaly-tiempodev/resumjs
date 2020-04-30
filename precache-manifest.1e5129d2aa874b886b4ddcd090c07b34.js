8821ca8d59c0977edfd83af5e63cbf3bb677ecc1
flkt <flkt.crnpio@gmail.com>
2020-04-30 15:39:53 -0500

    Updates

---
+++ precache-manifest.1e5129d2aa874b886b4ddcd090c07b34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38a3f5e2753c1d0d54aefab6e4f5d728",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abaf7e1eeb8fb149fe1f",
+    "url": "/static/css/main.951fb554.chunk.css"
+  },
+  {
+    "revision": "afea87c72e4d2cfd4351",
+    "url": "/static/js/2.005c30d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.005c30d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abaf7e1eeb8fb149fe1f",
+    "url": "/static/js/main.01ba767b.chunk.js"
+  },
+  {
+    "revision": "4885984d50a42209b6bb",
+    "url": "/static/js/runtime-main.f5e9834c.js"
+  }
+]);
